3a501e3e84e1b6058b8f05e44b000161fb7bd774
Matilda578 <101889190@student.swin.edu.au>
2020-04-07 20:12:03 +1000

    prints names and hello

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+
+        names[0] = "Coraline";
+        names[1] = "Jane";
+        names[2] = "Sarah";
+        names[3] = "Jake";
+        names[4] = "Felicity";
+
+        foreach(string s in names)
+        Console.WriteLine("Hello " + s);
+    
+
+        Console.ReadLine();
+        }
+    }
+}
